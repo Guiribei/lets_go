e1f1a3b29d1d73df2af594573552e45bc88c5e21
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-10-20 23:00:33 -0300

    incomplete

--- second/main.go
+++ second/main.go
@@ -7,4 +7,12 @@ func main() {
 	intNum = intNum + 1
 	fmt.Println(intNum)
 
+
+	var floatNum float64 = 12345678.9
+	fmt.Println(floatNum)
+
+	var floatNum32 float32 = 10.1
+	var intNum32 int32 = 2
+	var result float32 = floatNum32 + float32(intNum32)
+	fmt.Println(result)
 }
